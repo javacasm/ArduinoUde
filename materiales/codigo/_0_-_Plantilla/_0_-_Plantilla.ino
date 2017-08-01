0f7fc5dbe89809e870eef1880a0f8e1dca0d08c1
javacasm <javacasm@gmail.com>
2017-08-01 20:44:34 +0200

    plantilla

---
+++ materiales/codigo/_0_-_Plantilla/_0_-_Plantilla.ino
@@ -0,0 +1,41 @@
+/*
+Curso de Arduino
+
+Descripcion del proyecto
+
+Conexiones
+
+by JAVacasM
+ */
+
+
+/* ==== Includes - Librerias==== */
+
+/* ====  END Includes ==== */
+
+/* ==== Defines - Constantes ==== */
+/* ==== END Defines ==== */
+
+/* ==== Variables Globales ==== */
+
+/* ==== END Global Variables ==== */
+
+
+/* ==== Funciones a usar - Prototipos ==== */
+
+/* ==== END Prototipos ==== */
+
+/* ==== Setup - Configuracion ==== */
+void setup() {
+
+}
+/* ==== END Setup ==== */
+
+/* ==== Loop - Codigo que se repite==== */
+void loop() {
+}
+/* ==== End Loop ==== */
+
+/* ==== Funciones ==== */
+
+/* ==== END Functions ==== */
